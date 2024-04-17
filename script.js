76da243e8294a49351e3fbe500ca242e2ddf3207
Alexandre <alexandreliberatto@gmail.com>
2024-04-16 23:04:33 -0300

    finalizando o formulário e estilização completa

--- script.js
+++ script.js
@@ -4,21 +4,48 @@ const tabela = document.getElementById("tabela");
 
 let carros = [];
 
-frm.addEventListener("click", (e) => {
-e.preventDefault();
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-const model = frm.modelo.value;
-const plac = frm.placa.value;
-const corr = frm.cor.value;
-const propri = frm.proprietario.value;
-const aparta = Number(frm.apartemento.value);
-const bloc = frm.bloco.value;
-const vag = Number(frm.vaga.value);
+    const modelo = frm.modelo.value;
+    const placa = frm.placa.value;
+    const cor = frm.cor.value;
+    const proprietario = frm.proprietario.value;
+    const apartamento = Number(frm.apartamento.value);
+    const bloco = frm.bloco.value;
+    const vaga = Number(frm.vaga.value);
 
-const carro = { modelo, placa, cor, proprietario, apartamento, bloco, vaga };
-carros.push(carro);
+    const carro = { modelo, placa, cor, proprietario, apartamento, bloco, vaga };
+    carros.push(carro);
 
-frm.reset();
-atualizarTabela();
-
+    frm.reset();
+    atualizarTabela();
 });
+
+function atualizarTabela() {
+    tabela.innerHTML = `
+        <tr>
+            <th>Modelo</th>
+            <th>Placa</th>
+            <th>Cor</th>
+            <th>Proprietário</th>
+            <th>Apartamento</th>
+            <th>Bloco</th>
+            <th>Vaga</th>
+        </tr>
+    `;
+    
+    carros.forEach(carro => {
+        tabela.innerHTML += `
+            <tr>
+                <td>${carro.modelo}</td>
+                <td>${carro.placa}</td>
+                <td>${carro.cor}</td>
+                <td>${carro.proprietario}</td>
+                <td>${carro.apartamento}</td>
+                <td>${carro.bloco}</td>
+                <td>${carro.vaga}</td>
+            </tr>
+        `;
+    });
+}
